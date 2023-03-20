700ef44f53ab3df3ce5e3c97eae8d57255c6e226
brnkgabriel <brnkgabriel@gmail.com>
2023-03-20 17:19:56 +0100

    updated google analytics property

--- src/plugins/vue-gtag.client.ts
+++ src/plugins/vue-gtag.client.ts
@@ -6,12 +6,12 @@ export default defineNuxtPlugin(nuxtApp => {
   // Doing something with nuxtApp
   nuxtApp.vueApp.use(VueGtag, {
     property: {
-      id: "G-R6G45G34LW",
+      id: "G-938QWQ7WFN",
       params: {
         user_id: getMachineId(),
         send_page_view: false,
         linker: {
-          domain: ['tools-n-tuts.vercel.app']
+          domain: ['rcnlagos.netlify.app']
         }
       }
     }
