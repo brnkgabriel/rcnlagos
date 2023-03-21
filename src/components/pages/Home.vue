ed01f1a81deac9a3fc0966c5ff017d1db6d54827
brnkgabriel <brnkgabriel@gmail.com>
2023-03-21 01:44:53 +0100

    finished hero section

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,10 +1,98 @@
 <template>
-  <div> 
-    Homepage
+  <div>
+    <div class="-hero-banner">
+      <p class="-welcome">Welcome to</p>
+      <h1 class="-hero-header -headfont">RCN Lagos</h1>
+      <p class="-about px-4">We are a ministry with a single minded focus on restoring the prayer commission of the church
+        in fulfillment of
+        Jesus' proclamation in Matthew 21:13 that <strong>My house shall be called the house of prayer</strong></p>
+      <div class="-hero-ctas">
+        <a href="https://www.youtube.com/@RCNLagos/streams" class="-btn -stream-live">stream live</a>
+        <NuxtLink href="/about" class="-btn -know-more">know more</NuxtLink>
+      </div>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
 </script>
-<style lang="">
-  
+<style scoped>
+.-hero-banner {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 400px;
+  background: url("/images/cover.png") no-repeat;
+  background-position-y: 15%;
+  background-position-x: center;
+  background-size: 80%;
+  row-gap: 32px;
+  text-align: center;
+}
+
+.-welcome {
+  font-size: 29px;
+}
+
+.-hero-header {
+  font-size: 60px;
+  color: var(--rcnaccentblue);
+  font-weight: 800;
+}
+
+.-hero-banner .-about {
+  width: 70%;
+}
+
+.-stream-live {}
+
+.-know-more {
+  background-color: var(--rcnlighterbg);
+  box-shadow: 0px 4px 20px -5px var(--rcntext);
+  color: var(--rcntext) !important;
+}
+
+.-hero-ctas {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  column-gap: 8px;
+}
+
+.-hero-ctas a {
+  font-weight: 600;
+}
+
+@media screen and (max-width: 1024px) {}
+
+@media screen and (max-width: 768px) {}
+
+@media screen and (max-width: 576px) {
+  .-hero-banner {
+    row-gap: 8px;
+    background-size: cover;
+    background-position: center;
+    padding: 8px 0;
+    height: unset;
+  }
+
+
+  .-hero-banner .-about {
+    width: calc(100% - 32px);
+  }
+
+  .-hero-ctas {
+    column-gap: unset;
+    row-gap: 8px;
+    flex-direction: column;
+  }
+
+  .-hero-ctas,
+  .-hero-ctas a {
+    width: 100%;
+  }
+}
+
+@media screen and (max-width: 420px) {}
+
+@media screen and (min-width: 320px) and (max-width: 420px) {}
 </style>
--- src/components/partials/Header.vue
+++ src/components/partials/Header.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="flex justify-between items-center -header">
+  <div class="-header shadow-custom">
     <div class="-inner">
-      <div class="-logo">
+      <NuxtLink href="/" class="-logo">
         <img class="h-[40px]" src="/images/rcn-logo.png" alt="remnant christian network logo" />
-      </div>
+      </NuxtLink>
       <div class="-links">
         <NuxtLink href="/">home</NuxtLink>
         <NuxtLink href="/events">events</NuxtLink>
         <NuxtLink href="/blog">blog</NuxtLink>
         <NuxtLink href="/prayercells">prayer cells</NuxtLink>
-        <NuxtLink class="-btn" href="/give">give</NuxtLink>
+        <NuxtLink class="-btn -give" href="/give">give</NuxtLink>
       </div>
       <div class="-hamburger">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
@@ -24,6 +24,14 @@
 
 </script>
 <style scoped>
+
+.-header {
+  position: sticky;
+  left: 0;
+  top: 0;
+  z-index: 1;
+  background-color: white;
+}
 .-header .-inner {
   padding-top: 16px;
   padding-bottom: 16px;
@@ -46,6 +54,9 @@
   text-transform: uppercase;
   font-weight: 600;
   color: var(--rcntext);
+}
+.-header .-links .-give {
+  padding: 4px 16px;
 }
 
 .-header .-links a.router-link-active {
