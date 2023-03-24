c9f53d5aa16576aff48fb040b1f8766348cfd277
brnkgabriel <brnkgabriel@gmail.com>
2023-03-24 23:37:05 +0100

    updated partner page

--- src/pages/partner.vue
+++ src/pages/partner.vue
@@ -5,6 +5,12 @@
 </template>
 <script setup lang="ts">
 import Partner from '../components/pages/Partner.vue';
+definePageMeta({
+  layout: "default"
+})
+useHead(head({
+  name: "Partner | RCN Lagos",
+}))
 </script>
 <style scoped>
   
