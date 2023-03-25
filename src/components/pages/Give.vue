76591c46cd21c8e2b6269e6070237dbedc0b4c9c
brnkgabriel <brnkgabriel@gmail.com>
2023-03-25 01:56:00 +0100

    give page = 90%

--- src/components/pages/Give.vue
+++ src/components/pages/Give.vue
@@ -1,11 +1,376 @@
 <template>
-  <div>
-    Give
+  <div class="-give">
+    <div class="-inner">
+      <div class="-hero-banner relative">
+        <div class="-hero-title absolute">
+          <h1 class="-headfont">Give</h1>
+          <p>If you will like to be a part of what the LORD is doing at RCN Lagos, you're welcome to give using the
+            details
+            below</p>
+        </div>
+        <img class="-desktop" src="/images/prayercells_1511x495.jpg" alt="prayercell banner" />
+        <img class="-mobile" src="/images/prayercells_1000x495.jpg" alt="prayercell banner" />
+      </div>
+      <div class="-accounts">
+        <div class="-account -naira">
+          <h5 class="-subhead">Naira (NGN)</h5>
+          <div class="-table">
+            <p>Bank Name:</p>
+            <div class="-value">
+              <p>first bank of nigeria</p>
+              <div class="-icon -btn">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-4 h-4">
+                  <path
+                    d="M7.5 3.375c0-1.036.84-1.875 1.875-1.875h.375a3.75 3.75 0 013.75 3.75v1.875C13.5 8.161 14.34 9 15.375 9h1.875A3.75 3.75 0 0121 12.75v3.375C21 17.16 20.16 18 19.125 18h-9.75A1.875 1.875 0 017.5 16.125V3.375z" />
+                  <path
+                    d="M15 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0017.25 7.5h-1.875A.375.375 0 0115 7.125V5.25zM4.875 6H6v10.125A3.375 3.375 0 009.375 19.5H16.5v1.125c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 013 20.625V7.875C3 6.839 3.84 6 4.875 6z" />
+                </svg>
+              </div>
+            </div>
+            <p>Account Name:</p>
+            <div class="-value">
+              <p>remnant christian network ministry</p>
+              <div class="-icon -btn">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-4 h-4">
+                  <path
+                    d="M7.5 3.375c0-1.036.84-1.875 1.875-1.875h.375a3.75 3.75 0 013.75 3.75v1.875C13.5 8.161 14.34 9 15.375 9h1.875A3.75 3.75 0 0121 12.75v3.375C21 17.16 20.16 18 19.125 18h-9.75A1.875 1.875 0 017.5 16.125V3.375z" />
+                  <path
+                    d="M15 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0017.25 7.5h-1.875A.375.375 0 0115 7.125V5.25zM4.875 6H6v10.125A3.375 3.375 0 009.375 19.5H16.5v1.125c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 013 20.625V7.875C3 6.839 3.84 6 4.875 6z" />
+                </svg>
+              </div>
+            </div>
+            <p>Account Number:</p>
+            <div class="-value">
+              <p>2031730750</p>
+              <div class="-icon -btn">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-4 h-4">
+                  <path
+                    d="M7.5 3.375c0-1.036.84-1.875 1.875-1.875h.375a3.75 3.75 0 013.75 3.75v1.875C13.5 8.161 14.34 9 15.375 9h1.875A3.75 3.75 0 0121 12.75v3.375C21 17.16 20.16 18 19.125 18h-9.75A1.875 1.875 0 017.5 16.125V3.375z" />
+                  <path
+                    d="M15 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0017.25 7.5h-1.875A.375.375 0 0115 7.125V5.25zM4.875 6H6v10.125A3.375 3.375 0 009.375 19.5H16.5v1.125c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 013 20.625V7.875C3 6.839 3.84 6 4.875 6z" />
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="-account -dollars">
+          <h5 class="-subhead">Dollars (USD)</h5>
+          <div class="-table">
+            <p>Bank Name:</p>
+            <div class="-value">
+              <p>first bank of nigeria</p>
+              <div class="-icon -btn">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-4 h-4">
+                  <path
+                    d="M7.5 3.375c0-1.036.84-1.875 1.875-1.875h.375a3.75 3.75 0 013.75 3.75v1.875C13.5 8.161 14.34 9 15.375 9h1.875A3.75 3.75 0 0121 12.75v3.375C21 17.16 20.16 18 19.125 18h-9.75A1.875 1.875 0 017.5 16.125V3.375z" />
+                  <path
+                    d="M15 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0017.25 7.5h-1.875A.375.375 0 0115 7.125V5.25zM4.875 6H6v10.125A3.375 3.375 0 009.375 19.5H16.5v1.125c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 013 20.625V7.875C3 6.839 3.84 6 4.875 6z" />
+                </svg>
+              </div>
+            </div>
+            <p>Account Name:</p>
+            <div class="-value">
+              <p>remnant christian network ministry</p>
+              <div class="-icon -btn">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-4 h-4">
+                  <path
+                    d="M7.5 3.375c0-1.036.84-1.875 1.875-1.875h.375a3.75 3.75 0 013.75 3.75v1.875C13.5 8.161 14.34 9 15.375 9h1.875A3.75 3.75 0 0121 12.75v3.375C21 17.16 20.16 18 19.125 18h-9.75A1.875 1.875 0 017.5 16.125V3.375z" />
+                  <path
+                    d="M15 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0017.25 7.5h-1.875A.375.375 0 0115 7.125V5.25zM4.875 6H6v10.125A3.375 3.375 0 009.375 19.5H16.5v1.125c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 013 20.625V7.875C3 6.839 3.84 6 4.875 6z" />
+                </svg>
+              </div>
+            </div>
+            <p>Account Number:</p>
+            <div class="-value">
+              <p>2031730183</p>
+              <div class="-icon -btn">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-4 h-4">
+                  <path
+                    d="M7.5 3.375c0-1.036.84-1.875 1.875-1.875h.375a3.75 3.75 0 013.75 3.75v1.875C13.5 8.161 14.34 9 15.375 9h1.875A3.75 3.75 0 0121 12.75v3.375C21 17.16 20.16 18 19.125 18h-9.75A1.875 1.875 0 017.5 16.125V3.375z" />
+                  <path
+                    d="M15 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0017.25 7.5h-1.875A.375.375 0 0115 7.125V5.25zM4.875 6H6v10.125A3.375 3.375 0 009.375 19.5H16.5v1.125c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 013 20.625V7.875C3 6.839 3.84 6 4.875 6z" />
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="-account -pounds">
+          <h5 class="-subhead">Pounds (GBP)</h5>
+          <div class="-table">
+            <p>Bank Name:</p>
+            <div class="-value">
+              <p>first bank of nigeria</p>
+              <div class="-icon -btn">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-4 h-4">
+                  <path
+                    d="M7.5 3.375c0-1.036.84-1.875 1.875-1.875h.375a3.75 3.75 0 013.75 3.75v1.875C13.5 8.161 14.34 9 15.375 9h1.875A3.75 3.75 0 0121 12.75v3.375C21 17.16 20.16 18 19.125 18h-9.75A1.875 1.875 0 017.5 16.125V3.375z" />
+                  <path
+                    d="M15 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0017.25 7.5h-1.875A.375.375 0 0115 7.125V5.25zM4.875 6H6v10.125A3.375 3.375 0 009.375 19.5H16.5v1.125c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 013 20.625V7.875C3 6.839 3.84 6 4.875 6z" />
+                </svg>
+              </div>
+            </div>
+            <p>Account Name:</p>
+            <div class="-value">
+              <p>remnant christian network ministry</p>
+              <div class="-icon -btn">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-4 h-4">
+                  <path
+                    d="M7.5 3.375c0-1.036.84-1.875 1.875-1.875h.375a3.75 3.75 0 013.75 3.75v1.875C13.5 8.161 14.34 9 15.375 9h1.875A3.75 3.75 0 0121 12.75v3.375C21 17.16 20.16 18 19.125 18h-9.75A1.875 1.875 0 017.5 16.125V3.375z" />
+                  <path
+                    d="M15 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0017.25 7.5h-1.875A.375.375 0 0115 7.125V5.25zM4.875 6H6v10.125A3.375 3.375 0 009.375 19.5H16.5v1.125c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 013 20.625V7.875C3 6.839 3.84 6 4.875 6z" />
+                </svg>
+              </div>
+            </div>
+            <p>Account Number:</p>
+            <div class="-value">
+              <p>2031730217</p>
+              <div class="-icon -btn">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-4 h-4">
+                  <path
+                    d="M7.5 3.375c0-1.036.84-1.875 1.875-1.875h.375a3.75 3.75 0 013.75 3.75v1.875C13.5 8.161 14.34 9 15.375 9h1.875A3.75 3.75 0 0121 12.75v3.375C21 17.16 20.16 18 19.125 18h-9.75A1.875 1.875 0 017.5 16.125V3.375z" />
+                  <path
+                    d="M15 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0017.25 7.5h-1.875A.375.375 0 0115 7.125V5.25zM4.875 6H6v10.125A3.375 3.375 0 009.375 19.5H16.5v1.125c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 013 20.625V7.875C3 6.839 3.84 6 4.875 6z" />
+                </svg>
+              </div>
+
+            </div>
+          </div>
+        </div>
+        <div class="-account -euro">
+          <h5 class="-subhead">Euro (EUR)</h5>
+          <div class="-table">
+            <p>Bank Name:</p>
+            <div class="-value">
+              <p>first bank of nigeria</p>
+              <div class="-icon -btn">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-4 h-4">
+                  <path
+                    d="M7.5 3.375c0-1.036.84-1.875 1.875-1.875h.375a3.75 3.75 0 013.75 3.75v1.875C13.5 8.161 14.34 9 15.375 9h1.875A3.75 3.75 0 0121 12.75v3.375C21 17.16 20.16 18 19.125 18h-9.75A1.875 1.875 0 017.5 16.125V3.375z" />
+                  <path
+                    d="M15 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0017.25 7.5h-1.875A.375.375 0 0115 7.125V5.25zM4.875 6H6v10.125A3.375 3.375 0 009.375 19.5H16.5v1.125c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 013 20.625V7.875C3 6.839 3.84 6 4.875 6z" />
+                </svg>
+              </div>
+            </div>
+            <p>Account Name:</p>
+            <div class="-value">
+              <p>remnant christian network ministry</p>
+              <div class="-icon -btn">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-4 h-4">
+                  <path
+                    d="M7.5 3.375c0-1.036.84-1.875 1.875-1.875h.375a3.75 3.75 0 013.75 3.75v1.875C13.5 8.161 14.34 9 15.375 9h1.875A3.75 3.75 0 0121 12.75v3.375C21 17.16 20.16 18 19.125 18h-9.75A1.875 1.875 0 017.5 16.125V3.375z" />
+                  <path
+                    d="M15 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0017.25 7.5h-1.875A.375.375 0 0115 7.125V5.25zM4.875 6H6v10.125A3.375 3.375 0 009.375 19.5H16.5v1.125c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 013 20.625V7.875C3 6.839 3.84 6 4.875 6z" />
+                </svg>
+              </div>
+            </div>
+            <p>Account Number:</p>
+            <div class="-value">
+              <p>2031730200</p>
+              <div class="-icon -btn">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-4 h-4">
+                  <path
+                    d="M7.5 3.375c0-1.036.84-1.875 1.875-1.875h.375a3.75 3.75 0 013.75 3.75v1.875C13.5 8.161 14.34 9 15.375 9h1.875A3.75 3.75 0 0121 12.75v3.375C21 17.16 20.16 18 19.125 18h-9.75A1.875 1.875 0 017.5 16.125V3.375z" />
+                  <path
+                    d="M15 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0017.25 7.5h-1.875A.375.375 0 0115 7.125V5.25zM4.875 6H6v10.125A3.375 3.375 0 009.375 19.5H16.5v1.125c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 013 20.625V7.875C3 6.839 3.84 6 4.875 6z" />
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a href="#" class="-paystack -btn">paystack</a>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
 
+const { globalState } = useGlobals()
 </script>
-<style lang="">
-  
+<style scoped>
+.-give {
+  background-color: var(--rcnlightbg);
+}
+
+.-give>.-inner {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 16px;
+  padding-bottom: 16px;
+}
+
+.-give .-hero-banner {
+  background-color: black;
+  aspect-ratio: 1511 / 495;
+}
+
+.-give .-hero-banner .-hero-title {
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1;
+}
+
+.-give .-hero-banner .-hero-title h1,
+.-give .-hero-banner .-hero-title p {
+  color: white;
+  text-align: center;
+}
+
+.-give .-hero-banner .-hero-title h1 {
+  font-size: 60px;
+  font-weight: bold;
+}
+
+.-give img {
+  opacity: .2;
+}
+
+.-mobile {
+  display: none;
+}
+
+
+.-accounts {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 16px;
+  width: 100%;
+}
+
+.-accounts .-account {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 8px;
+  padding: 16px;
+  border-radius: 4px;
+  box-shadow: var(--box-shadow);
+  background-color: white;
+}
+
+.-accounts .-account.-naira .-subhead {
+  color: var(--green) !important;
+}
+
+.-accounts .-account.-dollars .-subhead {
+  color: var(--rcnaccentblue) !important;
+}
+
+.-accounts .-account.-pounds .-subhead {
+  color: var(--rcnaccentorange) !important;
+}
+
+.-accounts .-account.-euro .-subhead {
+  color: var(--red) !important;
+}
+
+.-accounts .-account .-subhead {
+  text-transform: uppercase;
+}
+
+.-accounts .-account .-table {
+  display: grid;
+  grid-template-columns: 25% 75%;
+  text-transform: capitalize;
+  gap: 8px;
+  width: 100%;
+}
+
+.-accounts .-account .-table>p {
+  text-align: right;
+}
+
+.-accounts .-account .-table .-value {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-weight: bold;
+}
+
+.-accounts .-account .-table>* {
+  padding: 8px;
+}
+
+.-accounts .-account .-table .-value>p {}
+
+
+.-accounts .-account.-naira .-table .-value .-icon {
+  background-color: var(--green);
+  box-shadow: 0px 4px 20px -5px var(--green);
+}
+
+.-accounts .-account.-dollars .-table .-value .-icon {
+  background-color: var(--rcnaccentblue);
+  box-shadow: 0px 4px 20px -5px var(--rcnaccentblue);
+}
+
+.-accounts .-account.-pounds .-table .-value .-icon {
+  background-color: var(--rcnaccentorange);
+  box-shadow: 0px 4px 20px -5px var(--rcnaccentorange);
+}
+
+.-accounts .-account.-euro .-table .-value .-icon {
+  background-color: var(--red);
+  box-shadow: 0px 4px 20px -5px var(--red);
+}
+.-accounts .-account .-table .-value .-icon {
+
+}
+
+.-paystack {
+  background-color: var(--paystack);
+  box-shadow: 0px 4px 20px -5px var(--paystack);
+}
+
+@media screen and (max-width: 1024px) {}
+
+@media screen and (max-width: 768px) {
+
+  .-give .-hero-banner .-hero-title h1 {
+    font-size: 40px
+  }
+
+  .-give .-hero-banner .-hero-title {
+    width: 90%;
+  }
+
+  .-mobile {
+    display: block;
+  }
+
+  .-desktop {
+    display: none;
+  }
+
+  .-give .-hero-banner {
+    background-color: black;
+    aspect-ratio: 1000 / 495;
+  }
+
+  .-accounts {
+    grid-template-columns: repeat(1, 1fr);
+  }
+}
+
+@media screen and (max-width: 576px) {
+
+  .-give .-hero-banner .-hero-title h1 {
+    font-size: 29px
+  }
+
+}
+
+@media screen and (max-width: 420px) {
+
+  .-give .-hero-banner .-hero-title p {
+    font-size: .8em;
+  }
+
+  .-give .-hero-banner .-hero-title {
+    width: calc(100% - 16px);
+  }
+}
+
+@media screen and (min-width: 320px) and (max-width: 420px) {}
 </style>
