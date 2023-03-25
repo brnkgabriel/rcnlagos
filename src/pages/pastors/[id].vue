72f2798fd2a0fa36bbb52e5a76fa2523723533eb
brnkgabriel <brnkgabriel@gmail.com>
2023-03-25 16:37:12 +0100

    updated pastor profile

--- src/pages/pastors/[id].vue
+++ src/pages/pastors/[id].vue
@@ -3,22 +3,12 @@
 </template>
 <script setup lang="ts">
 import PastorProfile from "~/components/pages/PastorProfile.vue"
-
-import { iPastorate } from '~~/src/types';
-const params = useRoute().params
-const id = params.id as string
-
-const { globalState } = useGlobals()
-
-const pastor = computed<iPastorate>(() => {
-  const pst = globalState.value.pastorates?.filter((pastor: iPastorate) => id.toLowerCase() === slug(pastor))[0]
-  return pst as iPastorate
-})
+ 
 definePageMeta({
   layout: "default"
 })
 useHead(head({
-  name: pastor.value ? `${pastor.value?.name} | RCN Lagos` : `Pastor | RCN Lagos`,
+  name: `Pastor | RCN Lagos`,
 }))
 </script>
 <style scoped>
