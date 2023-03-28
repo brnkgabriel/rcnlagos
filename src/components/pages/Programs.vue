461db7ff15c309aea18dc299c07e0add6391b871
brnkgabriel <brnkgabriel@gmail.com>
2023-03-28 12:02:01 +0100

    updated programs page

--- src/components/pages/Programs.vue
+++ src/components/pages/Programs.vue
@@ -18,9 +18,9 @@
         <img class="-mobile" src="/images/programs_1500x844.jpg" alt="prayercell banner" />
       </div>
       <div class="-catalog" v-infinite-scroll>
-        <!-- <Program v-if="isLoaded" v-for="(program, idx) in globalState.renderedPrograms" :key="idx" :program="program"
-          :class="(idx === globalState.renderedPrograms.length - 1) ? '-lastprogram' : ''" @selected="showProgram" /> -->
-        <sProgram v-for="(program, idx) in skeletonPrograms" :key="idx" :program="program" />
+        <Program v-if="isLoaded" v-for="(program, idx) in globalState.renderedPrograms" :key="idx" :program="program"
+          :class="(idx === globalState.renderedPrograms.length - 1) ? '-lastprogram' : ''" @selected="showProgram" />
+        <sProgram v-if="!isLoaded" v-for="(program, idx) in skeletonPrograms" :key="idx" :program="program" />
       </div>
     </div>
   </div>
