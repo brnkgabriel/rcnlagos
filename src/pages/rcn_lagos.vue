3b22331bc437eba2648974e448668629b642a8be
brnkgabriel <brnkgabriel@gmail.com>
2023-05-02 10:49:29 +0100

    added manual redirects by creating page components

---
+++ src/pages/rcn_lagos.vue
@@ -0,0 +1,15 @@
+<template>
+  <Home />
+</template>
+<script setup lang="ts"> 
+import Home from '../components/pages/Home.vue';
+definePageMeta({
+  layout: "default"
+})
+useHead(head({
+  name: "Home | RCN Lagos",
+}))
+</script>
+<style lang="">
+  
+</style>
