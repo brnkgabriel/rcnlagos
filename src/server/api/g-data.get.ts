bf0c4f79837657061b45c1716fdcb56f51e47f1f
brnkgabriel <brnkgabriel@gmail.com>
2023-05-05 19:14:12 +0100

    updated environment variables

--- src/server/api/g-data.get.ts
+++ src/server/api/g-data.get.ts
@@ -4,7 +4,8 @@ export default defineEventHandler(async (event: H3Event) => {
   try {
     const query = getQuery(event) as any
     // const path = query.path
-    const url = `https://script.google.com/macros/s/AKfycbyHcKzUKnpmc7yzz_6C_duR6qRcQjSXaSMxUH0Qh3zyGJOLhMptYzwCz9xGC1cVbQWp-w/exec`
+    const url = process.env.API_URL as string
+    
     const response = await fetch(url)
     const data = response.json()
     return data
